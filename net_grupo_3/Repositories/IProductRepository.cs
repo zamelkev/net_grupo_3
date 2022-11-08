98df4f82c1412619baed6b1acbfda044f7dc7051
jacintic <jacintiglesias@gmail.com>
2022-11-08 18:32:26 +0100

    in-progress/ products

--- net_grupo_3/Repositories/IProductRepository.cs
+++ net_grupo_3/Repositories/IProductRepository.cs
@@ -1,4 +1,18 @@
-﻿namespace net_grupo_3.Repositories {
-    public interface IProductRepository {
-    }
+﻿using System.Net;
+
+namespace net_grupo_3.Repositories; 
+public interface IProductRepository {
+
+    
+    Product FindById(int id);
+
+
+   /*
+    List<Product> FindAll();
+
+    // guardar
+    Product Create(Product product);
+
+    bool RemoveById(int id);
+   */
 }
