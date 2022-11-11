d7f67e79d63c4e559e49d4f9afe073e506d826a2
zamelkev <kevin.zamora.amela@gmail.com>
2022-11-11 13:18:52 +0100

    inProgress / Product & ProductComment clases

--- net_grupo_3/Models/Product.cs
+++ net_grupo_3/Models/Product.cs
@@ -2,7 +2,7 @@
 
 [Table("product")]
 public class Product {
-    // attrs
+    // atributos
     [Key, 
     Column("id")]
     public int Id { get; set; }
@@ -10,4 +10,41 @@ public class Product {
     [Required,
     Column("name")]
     public string Name { get; set; }
+
+    [Required,
+    Column("Cost")]
+    public Double Cost { get; set; }
+
+    [Required,
+    Column("price")]
+    public Double Price { get; set; }
+
+    [Required,
+    Column("stock")]
+    public int Stock { get; set; }
+
+    [Required,
+    Column("tax")]
+    public Double Tax { get; set; }
+
+    [Required,
+    Column("date")]
+    public DateTime ReleaseDate { get; set; }
+
+    [Required,
+    Column("comment")]
+    ProductComment? comment { get; set; }
+
+
+
+    public Product(int id, string name, double cost, double price, int stock, double tax, DateTime releaseDate, ProductComment? comment) {
+        Id = id;
+        Name = name;
+        Cost = cost;
+        Price = price;
+        Stock = stock;
+        Tax = tax;
+        ReleaseDate = releaseDate;
+        this.comment = comment;
+    }
 }
---
+++ net_grupo_3/Models/ProductComment.cs
@@ -0,0 +1,23 @@
+﻿namespace net_grupo_3.Models; 
+public class ProductComment {
+
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Body { get; set; }
+
+    public DateTime PostDate { get; set;}
+
+    public Product Product { get; set; }
+    public int ProductId { get; set; }
+
+    public ProductComment(int id, string title, string body, DateTime postDate, Product product, int productId) {
+        Id = id;
+        Title = title;
+        Body = body;
+        PostDate = postDate;
+        Product = product;
+        ProductId = productId;
+    }
+}
