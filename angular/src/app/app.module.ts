426615978eecd15dbbb8ce56a8a78880bf8c070a
zamelkev <kevin.zamora.amela@gmail.com>
2022-11-13 15:31:35 +0100

    in progress / Creación del componente "shop"

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsingleComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule
