723ed3f4fb7586aeb444777fd83938b4705f64ef
zamelkev <kevin.zamora.amela@gmail.com>
2022-11-14 16:50:31 +0100

    in progress / Eliminación de referencias a componentes eliminados

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { ProductsingleComponent } from './productsingle/productsingle.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ShopComponent } from './shop/shop.component';
@@ -14,7 +11,6 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-import { AddressComponent } from './address/address.component';
 import { EditAddressComponent } from './edit-address/edit-address.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -22,10 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
-    ProductsingleComponent,
     CartComponent,
     CheckoutComponent,
     ShopComponent,
@@ -34,7 +27,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     SignupComponent,
     ForgotPasswordComponent,
     ProfileDetailsComponent,
-    AddressComponent,
     EditAddressComponent,
     ProductDetailsComponent
   ],
