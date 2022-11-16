a1eb1259ab58c73a55aad55619da388db1b090c0
jacintic <jacintiglesias@gmail.com>
2022-11-16 18:01:40 +0100

    feature/ container filter fix/ product comment attributes

--- net_grupo_3/Models/ProductComment.cs
+++ net_grupo_3/Models/ProductComment.cs
@@ -1,12 +1,18 @@
-﻿namespace net_grupo_3.Models; 
+﻿namespace net_grupo_3.Models;
+[Table("product_comment")]
 public class ProductComment {
 
+    [Key,
+    Column("id")]
     public int Id { get; set; }
 
+    [Column("title")]
     public string Title { get; set; }
 
+    [Column("body")]
     public string Body { get; set; }
 
+    [Column("post_date")]
     public DateTime PostDate { get; set;}
     [JsonIgnore]
     public Product? Product { get; set; }
