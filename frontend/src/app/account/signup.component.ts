e288595bf75ce3bd6bc2f0c45dad4e3362e9d4b8
zamelkev <kevin.zamora.amela@gmail.com>
2023-01-06 19:39:04 +0100

    in-progress / Registrarse (Modificación inicial vista frontend)

--- frontend/src/app/account/signup.component.ts
+++ frontend/src/app/account/signup.component.ts
@@ -24,6 +24,7 @@ export class RegisterComponent implements OnInit {
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
             username: ['', Validators.required],
+            email: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]]
         });
     }
